29456cc4a947969aec3ab4b0c7e9e5a802c9c637
Francis Vo <francis@francis-laptop>
2010-01-10 15:23:33 -0800

    nothing

--- src/relationbrowser2/Frame.java
+++ src/relationbrowser2/Frame.java
@@ -10,19 +10,5 @@ import processing.core.*;
  * @author francis
  */
 public class Frame extends PApplet {
-    PFont fontA; //font used
-    Circle centerCircle = new Circle(); //ref to center circle
-    int currentColor, baseColor; // color of the background
-    int numCircle; //number of circles - same as circleUsed.length
-    float angles; // angle bewteen circles around the center circle
-    Circle[] Circles; // list of all circles
-    Circle[] circleUsed; // list of circles around center circle
-    int maxH = 150; // max distance from center circle
-    int speed = 20; // speed of movement
-    int H = maxH; // current distance from center circle
-    float centerX; //
-    float centerY;
-    Project currentProject = Project.current; //project used
-    boolean inaction = false; // flag for movement
-    Circle next; // next center circle
+    
 }
