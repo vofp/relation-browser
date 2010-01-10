62c33e29ad1a9eecfd82b6c3154c616855f85a2a
Francis Vo <francis@francis-laptop>
2010-01-10 12:12:11 -0800

    added

---
+++ src/relationbrowser2/Frame.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package relationbrowser2;
+import processing.core.*;
+/**
+ *
+ * @author francis
+ */
+public class Frame extends PApplet {
+
+}
