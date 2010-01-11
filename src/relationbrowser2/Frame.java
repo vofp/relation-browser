a3b859ff49d2e34e212744e08061c52f38469acf
Francis Vo <francis@francis-laptop>
2010-01-10 21:48:59 -0800

    added functions

--- src/relationbrowser2/Frame.java
+++ src/relationbrowser2/Frame.java
@@ -10,6 +10,18 @@ import processing.core.*;
  * @author francis
  */
 public class Frame extends PApplet {
+    PFont fontA; //font used
+    Circle[] Circles; // list of all circles
+    Project currentProject = Project.current; //project used
+    public void setup() {
+
+    }
+    public void draw() {
+
+    }
+    public void update(int x, int y) {
+        
+    }
     class Circle {
         
     }
